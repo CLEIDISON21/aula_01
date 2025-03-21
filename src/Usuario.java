ff0cef74c7923f3b31325967c1baf886e05bd24b
Cleidison Feitoza <118213344+CLEIDISON21@users.noreply.github.com>
2025-03-21 12:41:35 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,13 +2,16 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
-	smartTv.diminuirVolume();
-	smartTv.diminuirVolume();
-	smartTv.aumentarVolume();
-	System.out.println("Volume atual: " + smartTv.volume);
-	
 	System.out.println("TV ligada :" + smartTv.ligada);
 	System.out.println("Canal Atual :" + smartTv.canal);
 	System.out.println("Voliume atual: " + smartTv.volume);
+
+	smartTv.ligar();Novo status -> Tv ligada ? :" + smartTv.ligada); 
+	smartTv.diminuirVolume();
+	smartTv.diminuirVolume();
+	smartTv.aumentarVolume();
+	System.out.println("Volume atual: " + smartTv.volume);   
+	smartTv.desligar();Novo status -> Tv ligada ? :" + smartTv.desligada);     
+	    
     }
 }
